772b9f1c303259733f652eb6ed1ba5a97e455005
Jean-Baptiste Baillet <jbbaillet.dev@gmail.com>
2022-04-08 15:37:18 +0200

    production

--- pureBeurre/settings/production.py
+++ pureBeurre/settings/production.py
@@ -11,5 +11,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='purebeurre35@gmail.com'
-EMAIL_HOST_PASSWORD='Iotabeta85!'
+EMAIL_HOST_PASSWORD='Iotabeta85!purebeurre'
 EMAIL_USE_TLS=True
